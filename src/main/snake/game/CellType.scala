e59ae26a79ad383a17fcbadc4b8f855637491c51
Atze <atzeus@gmail.com>
2020-04-07 17:03:15 +0200

    Snake code

---
+++ src/main/snake/game/CellType.scala
@@ -0,0 +1,12 @@
+package snake.game
+
+sealed abstract class CellType
+
+case class SnakeHead(direction: Direction) extends CellType
+
+/** 0 is just before SnakeHead, 1.0 is tail */
+case class SnakeBody(distanceToHead: Float = 0f) extends CellType
+
+case class Empty() extends CellType
+
+case class Apple() extends CellType
---
+++ src/main/snake/logic/SnakeLogic.scala
@@ -0,0 +1,42 @@
+package snake.logic
+
+import engine.random.{RandomGenerator, ScalaRandomGen}
+import snake.game.{CellType, Direction, Empty}
+import snake.logic.SnakeLogic._
+
+/** To implement Snake, complete the ``TODOs`` below.
+ *
+ * If you need additional files,
+ * please also put them in the ``snake`` package.
+ */
+class SnakeLogic(val randomGen: RandomGenerator,
+                 val nrColumns: Int,
+                 val nrRows: Int) {
+
+  def this() = this(new ScalaRandomGen(), DefaultRows, DefaultColumns)
+
+  def isGameOver: Boolean = false
+
+  // TODO implement me
+  def step(): Unit = ()
+
+  // TODO implement me
+  def setReverseTime(reverse: Boolean): Unit = ()
+
+  // TODO implement me
+  def changeDir(d: Direction): Unit = ()
+
+  // TODO implement me
+  def getGridTypeAt(x: Int, y: Int): CellType = Empty()
+
+}
+
+/** SnakeLogic companion object */
+object SnakeLogic {
+
+  val DefaultColumns = 25
+  val DefaultRows = 25
+
+}
+
+
