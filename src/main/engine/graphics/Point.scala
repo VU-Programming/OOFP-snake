280db1006631790aec18d8e3ec25188cd5b31bd4
Atze <atzeus@gmail.com>
2020-05-12 11:08:34 +0200

    Improvements to base

--- src/main/engine/graphics/Point.scala
+++ src/main/engine/graphics/Point.scala
@@ -4,5 +4,6 @@
 package engine.graphics
 
 case class Point(x: Float, y: Float) {
-  def +(rhs: Point) = Point(x + rhs.x, y + rhs.y)
+  def +(rhs: Point) : Point = Point(x + rhs.x, y + rhs.y)
+  def -(rhs : Point) : Point = Point(x - rhs.x, y - rhs.y)
 }
