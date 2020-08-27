c0779646f2886d12672c27d3ae52485fb7fe162e
Anthony Wilkes <a.sjwilkes@gmail.com>
2020-08-27 16:41:50 +0200

    Reduce code repetition in test suites

--- src/test/snake/SnakeTestSuite.scala
+++ src/test/snake/SnakeTestSuite.scala
@@ -4,21 +4,11 @@ import infrastructure.ScoreCounter
 import org.junit.runner.RunWith
 import org.scalatest.{Args, Status, Suites}
 import org.scalatestplus.junit.JUnitRunner
-import snake.SnakeTestsAssignment1_1.MaxGrade
+import snake.BaseSnakeTestSuite.{BaseTests, MaxGrade}
 import snake.basic.{DeathTests, GrowthTests, MovementTests, NoReverseGameTests, NoReverseTest, PlacementTests, WrapAroundTests}
 import snake.reverse.ReverseTests
 
-@RunWith(classOf[JUnitRunner])
-class SnakeTestsAssignment1_1 extends Suites(
-    new PlacementTests,
-    new MovementTests,
-    new GrowthTests,
-    new WrapAroundTests,
-    new DeathTests,
-    new NoReverseTest,
-    new NoReverseGameTests
-) {
-
+abstract class SnakeTestSuite(suites: SnakeTestSuiteBase*) extends Suites(BaseTests ++ suites: _*) {
     override def run(testName: Option[String], args: Args): Status = {
         val scoreCounter = new ScoreCounter()
         val newArgs =
@@ -35,38 +25,24 @@ class SnakeTestsAssignment1_1 extends Suites(
     }
 }
 
-object SnakeTestsAssignment1_1 {
+object BaseSnakeTestSuite {
     val MaxGrade = 10
+    val BaseTests: Seq[SnakeTestSuiteBase] = Seq[SnakeTestSuiteBase](
+        new PlacementTests,
+        new MovementTests,
+        new GrowthTests,
+        new WrapAroundTests,
+        new DeathTests,
+        new NoReverseGameTests
+    )
 }
 
+@RunWith(classOf[JUnitRunner])
+class SnakeTestsAssignment1_1 extends SnakeTestSuite(
+    new NoReverseTest
+)
 
 @RunWith(classOf[JUnitRunner])
-class SnakeTestsAssignment1_3 extends Suites(
-    new PlacementTests,
-    new MovementTests,
-    new GrowthTests,
-    new WrapAroundTests,
-    new DeathTests,
-    new NoReverseGameTests,
+class SnakeTestsAssignment1_3 extends SnakeTestSuite(
     new ReverseTests
-) {
-
-    override def run(testName: Option[String], args: Args): Status = {
-        val scoreCounter = new ScoreCounter()
-        val newArgs =
-            args.copy(configMap = args.configMap.updated("scoreCounter",scoreCounter))
-        val res = runDirect(testName,newArgs)
-        printf("You got %d/%d points!\n", scoreCounter.points, scoreCounter.maxPoints)
-        printf("Your grade for the repls exercise will be : %.2f\n",scoreCounter.fraction() * MaxGrade)
-        res
-    }
-
-    // run without making a new scorecounter
-    def runDirect(testName: Option[String], args: Args): Status = {
-        super.run(testName, args)
-    }
-}
-
-object SnakeTestsAssignment1_3 {
-    val MaxGrade = 10
-}
+)
