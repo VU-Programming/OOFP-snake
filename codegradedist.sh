ec2b460bb0930977fdf08db3402e5247ba5bf295
Atze <atzeus@gmail.com>
2020-08-31 10:14:45 +0200

    Fix codegrade infra

---
+++ codegradedist.sh
@@ -0,0 +1,9 @@
+mkdir -p codegradedist
+cd codegradedist
+wget https://gitlab.com/vu-oofp/gamebase/-/archive/snake/gamebase-snake.tar.gz
+tar -xvf gamebase-snake.tar.gz --strip-components=1
+rm gamebase-snake.tar.gz
+rm -r lib
+rm -r src/main/snake/game/
+rm -r src/main/engine/GameBase.scala src/main/engine/graphics/
+tar cvzf snake.tar.gz *
