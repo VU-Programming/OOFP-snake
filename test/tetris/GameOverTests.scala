ebe3549cecae4eaab99b8c50db751c4c20646d8c
Atze <atzeus@gmail.com>
2022-10-14 10:42:26 +0200

    Test tight fit by Andrzej Kominek

--- test/tetris/GameOverTests.scala
+++ test/tetris/GameOverTests.scala
@@ -69,4 +69,23 @@ class GameOverTests extends TetrisTestSuiteBase {
       , "You cannot escape from a game over state by moving or rotating blocks.")
   }
 
+  test("testNoGameOverTightFit") {
+    checkGame(initialBoard =
+      """I..I
+        |I..I
+        |I..I
+        |I..I""",
+      List(TestFrame(3,
+        """IOOI
+          |IOOI
+          |I..I
+          |I..I"""),
+        TestFrame(3, List(Down),
+          """I..I
+            |IOOI
+            |IOOI
+            |I..I"""))
+      , "GameOver should happen only when the new spawning block collides with any placed block.")
+  }
+
 }
